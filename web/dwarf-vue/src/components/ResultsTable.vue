4aae1b2188aeb1e6b1c06b5ea6b0156946e47d40
jonhteper <jonhteper@protonmail.com>
2023-07-21 15:18:04 -0600

    fix: valores por default en tabla de resultados

--- web/dwarf-vue/src/components/ResultsTable.vue
+++ web/dwarf-vue/src/components/ResultsTable.vue
@@ -9,38 +9,39 @@ const props = defineProps(['results']);
     <div class="ResultsTable">
         <h2>Resultados</h2>
         <table>
-            <Row title="Entrada" :value="props?.results?.input || '0'"/>
-            <Row title="IVA" :value="props?.results?.iva || '0'"/>
-            <Row title="ISR" :value="props?.results?.isr || '0'"/>
-            <Row title="Libre de impuestos" :value="props?.results?.taxes_free || '0'"/>
-            <Row title="Subtotal" :value="props?.results?.subtotal || '0'"/>
-            <Row title="Total de la factura" :value="props?.results?.total || '0'"/>
+            <Row title="Entrada" :value="props?.results?.input || '$ 0'" />
+            <Row title="IVA" :value="props?.results?.iva || '$ 0'" />
+            <Row title="ISR" :value="props?.results?.isr || '$ 0'" />
+            <Row title="Libre de impuestos" :value="props?.results?.taxes_free || '$ 0'" />
+            <Row title="Subtotal" :value="props?.results?.subtotal || '$ 0'" />
+            <Row title="Total de la factura" :value="props?.results?.total || '$ 0'" />
         </table>
         <button>Guardar</button>
     </div>
 </template>
 
 <style>
-    .ResultsTable {
-        display: inline-grid;
-        text-align: center;
-    }
+.ResultsTable {
+    display: inline-grid;
+    text-align: center;
+}
 
-    .ResultsTable table {
-        margin: auto;
-        width: 90%;
-        padding: 1%;
-    }
-    .ResultsTable table th {
-        text-align: left;
-    }
+.ResultsTable table {
+    margin: auto;
+    width: 90%;
+    padding: 1%;
+}
 
-    .ResultsTable table td {
-        text-align: right;
-    }
+.ResultsTable table th {
+    text-align: left;
+}
 
-    .ResultsTable button {
-        width: 90%;
-        margin: auto;
-    }
+.ResultsTable table td {
+    text-align: right;
+}
+
+.ResultsTable button {
+    width: 90%;
+    margin: auto;
+}
 </style>
